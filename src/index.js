74aba02086f244e4a160805217d67eecdf6e72db
karymefatima <karymecrisologo@gmail.com>
2024-07-29 10:53:26 +0800

    index

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import HomePage from './HomePage';
+import HomePageUI from './HomePageUI';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HomePage/>
+      <HomePageUI/>
   </React.StrictMode>
 );
 
