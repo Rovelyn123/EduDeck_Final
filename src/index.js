a8cefa82879c4928a1e59b0dc2196d47c20d0e11
karymefatima <karymecrisologo@gmail.com>
2024-07-29 10:52:16 +0800

    removed unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Login from './Login';
-import QuizSession from './QuizSession';
-import AboutUs from './AboutUs';
-import FlashcardManagement from './FlashcardManagement';
-import FlashcardProgress from './FlashcardProgress';
-import Pricing from './Pricing';
-import { BrowserRouter } from 'react-router-dom';
 import HomePage from './HomePage';
-import App from './App';
-import FlashcardAnalytics from "./FlashcardAnalytics";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
-    {/* <App /> */}
       <HomePage/>
-    {/* <QuizSession/> */}
-    {/* <Dashboard/> */}
-    {/* <UploadDocument/> */}
-    {/* <FlashcardProgress/> */}
-    {/* <App /> */}
-    {/* <LearningSession/> */}
-    {/* <AboutAcadZen/> */}
-    {/* <AboutUs/> */}
-    {/* <Pricing/> */}
-    {/* </BrowserRouter> */}
-    {/* <FlashcardAnalytics/> */}
-
   </React.StrictMode>
 );
 
